55c52bc5aa1ab2b5115985978a3e51e21e433a29
來紀 <490719815@qq.com>
2024-11-16 18:00:51 +0800

    .

--- 6/main.go
+++ 6/main.go
@@ -1,41 +1,61 @@
+// package main
+
+// import (
+// 	"fmt"
+// 	"runtime"
+// 	"sync"
+// )
+
+// var flag = true
+
+// var wg sync.WaitGroup
+
+// func get(ch chan int) {
+// 	num := 0
+// 	ok := true
+// 	for ok {
+// 		_, flag = <-ch
+// 		num++
+// 		if flag == false {
+// 			break
+// 		}
+// 	}
+// 	fmt.Println("Moved ", num)
+
+// }
+
+// func main() {
+// 	runtime.GOMAXPROCS(10)
+// 	cap := make(chan int, 2000)
+// 	//1 -> 2000 pool
+// 	go func() {
+// 		for i := 0; i < 2000; i++ {
+// 			cap <- 1
+// 		}
+// 	}()
+
+// 	//car
+// 	cars := make(chan int, 10)
+
+// 	wg.Wait()
+// }
+
 package main
 
-import (
-	"fmt"
-	"runtime"
-	"sync"
-)
-
-var flag = true
-
-var wg sync.WaitGroup
-
 func get(ch chan int) {
-	num := 0
-	ok := true
-	for ok {
-		_, flag = <-ch
-		num++
-		if flag == false {
-			break
-		}
-	}
-	fmt.Println("Moved ", num)
+	flag := true
 
 }
-
 func main() {
-	runtime.GOMAXPROCS(10)
-	cap := make(chan int, 2000)
-	//1 -> 2000 pool
+
+	ch := make(chan int, 2000)
+	for i := 0; i < 2000; i++ {
+		ch <- 1
+	}
+
 	go func() {
-		for i := 0; i < 2000; i++ {
-			cap <- 1
+		for i := 0; i < 10; i++ {
+			get(ch)
 		}
 	}()
-
-	//car
-	cars := make(chan int, 10)
-
-	wg.Wait()
 }
